dd54d21fbe6aca6d89c5a1d426c6e8acab9ed01b
jcampolim <121445600+jcampolim@users.noreply.github.com>
2024-02-27 18:21:48 -0300

    Update CarroDePasseio.java

--- heranca/src/CarroDePasseio.java
+++ heranca/src/CarroDePasseio.java
@@ -1,39 +1,38 @@
-//Nome / RA: 
-//Enzo - 10410074
-//Erika - 10403716
-//Júlia - 10408802
+// Nome / RA:
+// Enzo Guarnieri - 10410074
+// Erika Borges Piaui - 10403716
+// Júlia Campolim de Oste- 10408802
 
-//Fontes utilizadas para esse trabalho: 
+// Fontes utilizadas para esse trabalho:
 // Material disponibilizado pelo professor.
 // e os seguintes sites;
 // - https://www.devmedia.com.br/trabalhando-com-arrays-em-java/25530
 // - https://pt.stackoverflow.com/questions/3905/como-comparar-strings-em-java
 
-
 public class CarroDePasseio extends VeiculoTerrestre{
-	private int totalDePassageiros;
-	
-		//Construtores
-		public CarroDePasseio(){
-			super();
-			totalDePassageiros = 0;
-			
-		}
-		public CarroDePasseio(int ano, String cor, int totalDePassageiros){
-			super(ano, cor);
-			this.totalDePassageiros = totalDePassageiros;
-		}
-		
-		//Getters e Setters
-		public int getTotalDePassageiros() {
-			return totalDePassageiros;
-		}
-		public void setTotalDePassageiros(int totalDePassageiros) {
-			this.totalDePassageiros = totalDePassageiros;
-		}
-		
-		// Override do metodo toString
-		public String toString() {
-			return "Carro de Passeio: ano " + super.getAno() + "," + " cor " + super.getCor() + "," + " Total de Passageiros " + totalDePassageiros; 
-		}
+    private int totalDePassageiros;
+
+    //Construtores
+    public CarroDePasseio(){
+        super();
+        totalDePassageiros = 0;
+
+    }
+    public CarroDePasseio(int ano, String cor, int totalDePassageiros){
+        super(ano, cor);
+        this.totalDePassageiros = totalDePassageiros;
+    }
+
+    //Getters e Setters
+    public int getTotalDePassageiros() {
+        return totalDePassageiros;
+    }
+    public void setTotalDePassageiros(int totalDePassageiros) {
+        this.totalDePassageiros = totalDePassageiros;
+    }
+
+    // Override do metodo toString
+    public String toString() {
+        return "Carro de Passeio: ano " + super.getAno() + "," + " cor " + super.getCor() + "," + " Passageiros " + totalDePassageiros;
+    }
 }
