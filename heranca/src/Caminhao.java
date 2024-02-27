492d24fb8baee166010f0973b541a8018317e5b4
jcampolim <121445600+jcampolim@users.noreply.github.com>
2024-02-27 18:21:32 -0300

    Update Caminhao.java

--- heranca/src/Caminhao.java
+++ heranca/src/Caminhao.java
@@ -1,38 +1,39 @@
-//Nome / RA: 
-//Enzo - 10410074
-//Erika - 10403716
-//Júlia - 10408802
+// Nome / RA:
+// Enzo Guarnieri - 10410074
+// Erika Borges Piaui - 10403716
+// Júlia Campolim de Oste- 10408802
 
-//Fontes utilizadas para esse trabalho: 
+// Fontes utilizadas para esse trabalho:
 // Material disponibilizado pelo professor.
 // e os seguintes sites;
 // - https://www.devmedia.com.br/trabalhando-com-arrays-em-java/25530
 // - https://pt.stackoverflow.com/questions/3905/como-comparar-strings-em-java
 
 public class Caminhao extends VeiculoTerrestre{
-	private int totalDeEixos;
-	
-	//Construtores
-	public Caminhao(){
-		super();
-		totalDeEixos = 0;
-				
-	}
-	public Caminhao(int ano, String cor, int totalDeEixos){
-		super(ano, cor);
-		this.totalDeEixos = totalDeEixos;
-	}
-	
-		//Getters e Setters
-		public int getTotalDeEixos() {
-			return totalDeEixos;
-		}
-		public void setTotalDeEixos(int totalDeEixos) {
-			this.totalDeEixos = totalDeEixos;
-		}
-		
-		// Override do metodo toString
-		public String toString() {
-			return "Carro de Passeio: ano " + super.getAno() + "," + " cor " + super.getCor() + "," + " Total de Eixos " + totalDeEixos; 
-		}
+    private int totalDeEixos;
+
+    //Construtores
+    public Caminhao(){
+        super();
+        totalDeEixos = 0;
+
+    }
+    
+    public Caminhao(int ano, String cor, int totalDeEixos){
+        super(ano, cor);
+        this.totalDeEixos = totalDeEixos;
+    }
+
+    //Getters e Setters
+    public int getTotalDeEixos() {
+        return totalDeEixos;
+    }
+    public void setTotalDeEixos(int totalDeEixos) {
+        this.totalDeEixos = totalDeEixos;
+    }
+
+    // Override do metodo toString
+    public String toString() {
+        return "Carro de Passeio: ano " + super.getAno() + "," + " cor " + super.getCor() + "," + " Eixos " + totalDeEixos;
+    }
 }
