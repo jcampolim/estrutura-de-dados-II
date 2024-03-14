4bb4eed5abde006f20533eafb4972bff7e30bdc0
jcampolim <julia.campolim@gmail.com>
2024-03-14 10:14:47 -0300

    Operação insert e clear da BST

--- arvore-binaria-de-busca/src/BST.java
+++ arvore-binaria-de-busca/src/BST.java
@@ -1,3 +1,5 @@
+// https://sentry.io/answers/how-to-compare-strings-in-java/#:~:text=If%20you%20want%20to%20find,compareTo()%20.
+
 public class BST extends BinaryTree {
     public BST() {
         super();
@@ -19,8 +21,40 @@ public class BST extends BinaryTree {
     // Opcional: lançar execeção (throw new RuntimeException()) para indicar que
     // já existe um nó com a chave indicada
     // julia
-    public boolean insert(String data) {
-        return true;
+    public Node insert(String data) {
+        if(isEmpty()) {
+            root = new Node(data, null);
+            return root;
+        } else {
+            Node node = new Node(data, null);
+            insert(root, node, data);
+
+            return node;
+        }
+    }
+
+    public void insert(Node root, Node node, String data) {
+        if(data.compareTo(root.getData()) < 0) {
+            if(root.getLeft() == null) {
+                root.setLeft(node);
+                node.setParent(root);
+
+                return;
+            }
+
+            insert(root.getLeft(), node, data);
+        } else if(data.compareTo(root.getData()) > 0) {
+            if(root.getRight() == null) {
+                root.setRight(node);
+                node.setParent(root);
+
+                return;
+            }
+
+            insert(root.getRight(), node, data);
+        } else if(data.compareTo(root.getData()) == 0) {
+            return;
+        }
     }
 
     //TODO: remove nó, caso o nó pertença à BST.
@@ -59,8 +93,22 @@ public class BST extends BinaryTree {
 
     //TODO: remove todos os nós e suas conexões
     // julia
-    public boolean clear() {
-        return true;
+    public void clear() {
+        clear(root);
+    }
+
+    public void clear(Node root) {
+        if(root.getLeft() != null) {
+            clear(root.getLeft());
+            root.setLeft(null);
+        }
+        if(root.getRight() != null) {
+            clear(root.getRight());
+            root.setRight(null);
+        }
+
+        root.setParent(null);
+        root.setData(null);
     }
 
     //TODO: fazer toString
--- arvore-binaria-de-busca/src/Main.java
+++ arvore-binaria-de-busca/src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BST bst = new BST();
+
+        bst.insert("08");
+        bst.insert("03");
+        bst.insert("01");
+        bst.insert("06");
+        bst.insert("04");
+        bst.insert("07");
+        bst.insert("10");
+        bst.insert("14");
+
+        bst.inOrderTraversal();
+
+        bst.clear();
+        bst.inOrderTraversal();
     }
 }
