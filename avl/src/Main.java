1c1fb26f01b0390d1feeca8cc4424d1918904541
jcampolim <julia.campolim@gmail.com>
2024-04-10 22:39:02 -0300

    Atualização da main

--- avl/src/Main.java
+++ avl/src/Main.java
@@ -2,60 +2,65 @@ public class Main {
 
 
     public static void main(String[] args) {
-        AVL tree = new AVL();
-
         System.out.println("a) Inserir nós com chaves 1, 2 e 3 (nesta sequência).");
-        tree.insert(1);
-        tree.insert(2);
-        tree.insert(3);
-        tree.treeInfo();
-        // aplicar rotacoes
 
-        tree.treeInfo();
-        // limpar a arvore
-        tree.remove(1);
-        tree.remove(2);
-        tree.remove(3);
+        AVL treeA = new AVL();
 
-        System.out.println("b) Inserir nós com chaves 3, 2 e 1 (nesta sequência).");
-        tree.insert(3);
-        tree.insert(2);
-        tree.insert(1);
-        tree.treeInfo();
-        // aplicar rotacoes
+        treeA.insert(1);
+        treeA.insert(2);
+        treeA.insert(3);
 
-        tree.treeInfo();
-        // limpar a arvore
-        tree.remove(1);
-        tree.remove(2);
-        tree.remove(3);
+        System.out.println(" > Antes da rotação: ");
+        treeA.treeInfo();
 
-        System.out.println("c) Inserir nós com chaves 3, 1 e 2 (nesta sequência).");
-        tree.insert(3);
-        tree.insert(1);
-        tree.insert(2);
-        tree.treeInfo();
-        // aplicar rotacoes
+        // rotateLeft
+        System.out.println("\n > Depois da rotação: ");
+        treeA.treeInfo();
 
-        tree.treeInfo();
-        // limpar a arvore
-        tree.remove(1);
-        tree.remove(2);
-        tree.remove(3);
+        System.out.println("\nb) Inserir nós com chaves 3, 2 e 1 (nesta sequência).");
 
-        System.out.println("d) Inserir nós com chaves 1, 3 e 2 (nesta sequência).");
-        tree.insert(1);
-        tree.insert(3);
-        tree.insert(2);
-        tree.treeInfo();
-        // aplicar rotacoes
+        AVL treeB = new AVL();
 
-        tree.treeInfo();
-        // limpar a arvore
-        tree.remove(1);
-        tree.remove(2);
-        tree.remove(3);
+        treeB.insert(3);
+        treeB.insert(2);
+        treeB.insert(1);
 
+        System.out.println(" > Antes da rotação: ");
+        treeB.treeInfo();
+
+        treeB.rotateRight();
+        System.out.println("\n > Depois da rotação: ");
+        treeB.treeInfo();
+
+        System.out.println("\nc) Inserir nós com chaves 3, 1 e 2 (nesta sequência).");
+
+        AVL treeC = new AVL();
+
+        treeC.insert(3);
+        treeC.insert(1);
+        treeC.insert(2);
+
+        System.out.println(" > Antes da rotação: ");
+        treeC.treeInfo();
+
+        // rotateLeftRight
+        System.out.println("\n > Depois da rotação: ");
+        treeC.treeInfo();
+
+        System.out.println("\nd) Inserir nós com chaves 1, 3 e 2 (nesta sequência).");
+
+        AVL treeD = new AVL();
+
+        treeD.insert(1);
+        treeD.insert(3);
+        treeD.insert(2);
+
+        System.out.println(" > Antes da rotação: ");
+        treeD.treeInfo();
+
+        treeD.rotateRightLeft();
+        System.out.println("\n > Depois da rotação: ");
+        treeD.treeInfo();
 
     }
 }
