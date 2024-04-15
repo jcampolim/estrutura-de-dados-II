aac3c29682f242cf2f70bc9a272222925c9371de
EBP_erika <93957694+ErikaBP@users.noreply.github.com>
2024-04-14 21:22:43 -0300

    Método rotateLeft e rotateLeftRight

--- avl/src/AVL.java
+++ avl/src/AVL.java
@@ -9,6 +9,26 @@ public class AVL extends BST {
 
     // TODO: método ll (rotateLeft) - erika
 
+    public void rotateLeft() {
+        rotateLeft(root);
+    }
+
+    public void rotateLeft(Node oldRoot) {
+        root = oldRoot.getRight();
+
+        oldRoot.setParent(root);
+        root.setParent(null);
+
+        if(root.getLeft() != null) {
+            oldRoot.setRight(root.getLeft());
+            root.getLeft().setParent(oldRoot);
+        } else {
+            oldRoot.setRight(null);
+        }
+
+        root.setLeft(oldRoot);
+    }
+
     public void rotateRight() {
         rotateRight(root);
     }
@@ -30,7 +50,40 @@ public class AVL extends BST {
     }
 
     // TODO: método lr (rotateLeftRight) - erika
-
+    public void rotateLeftRight() {
+            rotateLeftRight(root);
+        }
+        
+    public void rotateLeftRight(Node oldRoot) {
+            Node aux = root.getLeft();
+            Node newRoot = aux.getRight();
+    
+            oldRoot.setLeft(newRoot);
+            aux.setParent(newRoot);
+    
+            if(newRoot.getLeft() != null) {
+                aux.setRight(newRoot.getLeft());
+                newRoot.getLeft().setParent(aux);
+            } else {
+                aux.setRight(null);
+            }
+    
+            newRoot.setLeft(aux);
+    
+            root = newRoot;
+            oldRoot.setParent(newRoot);
+            newRoot.setParent(null);
+    
+            if(newRoot.getRight() != null) {
+                oldRoot.setLeft(newRoot.getRight());
+                newRoot.getRight().setParent(oldRoot);
+            } else {
+                oldRoot.setLeft(null);
+            }
+    
+            newRoot.setRight(oldRoot);
+        }
+    
     public void rotateRightLeft() {
         rotateRightLeft(root);
     }
