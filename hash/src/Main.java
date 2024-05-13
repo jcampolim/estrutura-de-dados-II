88ae9a28a8665f7df7678a18cbf5b877fce9032b
EBP_erika <93957694+ErikaBP@users.noreply.github.com>
2024-05-13 11:30:11 -0300

    Testes HashTableOA

--- hash/src/Main.java
+++ hash/src/Main.java
@@ -1,9 +1,23 @@
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("Testes para HashTable de endereçamento aberto: ");
+      
         HashTableOA teste1 = new HashTableOA(3);
+  
+        System.out.println("Busca por valor 0, que não existe: " + teste1.search(0));
 
+        System.out.println("Remoção do valor 0, que não existe: " + teste1.remove(0));
 
+        System.out.println("Inserir valor (0,0): " + teste1.insert(0, "0"));
 
+        System.out.println("Inserir valor (0,1): " + teste1.insert(0, "1"));
+
+        System.out.println("Enchendo com os valores (1,1), (2,2), (3,3) e (4,4):");
+        teste1.insert(1, "1");
+        teste1.insert(2, "2");
+        teste1.insert(3, "3");
+        teste1.insert(4, "4");
 
         System.out.println("Testes para HashTable ligada: ");
 
@@ -29,14 +43,6 @@ public class Main {
         System.out.println("Busca por chave 2: " + teste2.search(2));
         System.out.println("Busca por chave 3: " + teste2.search(3));
         System.out.println("Busca por chave 4: " + teste2.search(4));
-
-
-
-
-
-
-
-
 
     }
 }
